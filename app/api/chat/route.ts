48a8441720e0a9387bf910471bf7aa40d9cf7084
Osman Minaz <osman.minaz@t-systems.com>
2024-03-29 01:51:46 +0100

    feat: f

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,13 +1,13 @@
-import { StreamingTextResponse, experimental_streamText } from 'ai';
-import { openai } from 'ai/openai';
+import { StreamingTextResponse, experimental_streamText } from "ai";
+import { openai } from "ai/openai";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
   const result = await experimental_streamText({
-    model: openai.chat('gpt-4-turbo-preview'),
+    model: openai.chat("gpt-3.5-turbo"),
     messages,
   });
 
